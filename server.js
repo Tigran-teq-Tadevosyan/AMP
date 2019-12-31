2087821a3fb9ed6713194373501aead41db8c7e3
Tigran <tigran@tadevosyan.net>
2019-12-31 07:01:11 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const uuidv1 = require("uuid/v1");
 const fs = require("fs");
 
